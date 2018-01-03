6bb26eef299903b19cf86c68996893c4602b9f39
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2018-01-03 23:04:09 +0700

    added bx api

--- app.js
+++ app.js
@@ -11,26 +11,53 @@ app.use(bodyParser.json())
 
 var api = axios.create({
     baseURL: 'https://api.line.me/v2/bot',
-    headers : {'Content-Type':'application/json','Authorization':'Bearer KFPq5HxMg3+OxUlw1ArdTNS76/x8Hq025lV0EcJmosKab8m9oup1svrubKY7qurzhCqdY+fOKvc/rBCZk1pfG22mf/PaNY7Yy5n6GaWpGo23VjB/p0otBT8NuCQiBrrX6NktUHoZXiyWfFHhJB3DGgdB04t89/1O/w1cDnyilFU='}
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer KFPq5HxMg3+OxUlw1ArdTNS76/x8Hq025lV0EcJmosKab8m9oup1svrubKY7qurzhCqdY+fOKvc/rBCZk1pfG22mf/PaNY7Yy5n6GaWpGo23VjB/p0otBT8NuCQiBrrX6NktUHoZXiyWfFHhJB3DGgdB04t89/1O/w1cDnyilFU='
+    }
 })
+
+function replyText(replyToken,coinName,coinPrice,coinUnit) {
+    let replyData = {
+        replyToken: replyToken,
+        messages: [{
+            type: 'text',
+            text: coinName + ' : ' + coinPrice + ' ' + coinUnit
+        }]
+    }
+    api.post('/message/reply', replyData)
+        .then((res) => {
+            console.log("reply success")
+        })
+        .catch((error) => {
+            console.log("reply error")
+        })
+}
+
 function analyzeText(message) {
     var text = message.message.text
     var sender = message.source.userId
     var replyToken = message.replyToken
-    let replyData = {
-        replyToken: replyToken,
-        messages : [{
-            type: 'text',
-            text: 'Reply' + text
-        }]
-    }
-    api.post('/message/reply',replyData)
-        .then((res)=>{
-            console.log("reply success")
+    text = text.toUpperCase()
+    getBXCurrency(replyToken,text)
+}
+
+function getBXCurrency(replyToken,currency) {
+    axios.get('https://bx.in.th/api/')
+        .then((res) => {
+            Object.entries(res.data).forEach(([coin_id,coin])=>{
+                let coinName = coin.secondary_currency
+                let coinUnit = coin.primary_currency
+                if(coinName == currency){
+                    let coinPrice = coin.last_price
+                    replyText(replyToken,coinName,coinPrice,coinUnit);
+                }
+            })
         })
-        .catch((error)=>{
-            console.log("reply error")
+        .catch((error) => {
+            console.log(error)
         })
+
 }
 
 app.post('/webhook', (req, res) => {
@@ -38,6 +65,10 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(200)
 })
 
+app.get('/getBX',(req,res)=>{
+    getBXCurrency()
+    res.sendStatus(200)
+})
 
 app.listen(app.get('port'), function () {
     console.log('LINE WEBHOOK RUN AT PORT', app.get('port'))
